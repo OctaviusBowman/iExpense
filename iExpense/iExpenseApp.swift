26f752451496294356018b4db2df9d20546d2688
Octavius Bowman <octaviusbowman@gmail.com>
2021-11-01 16:54:54 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Octavius Bowman on 11/1/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
